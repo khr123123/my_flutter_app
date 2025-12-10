5ad2dd865787a7539f53c9ca1721c8466d62360e
3031208 <21@qwe>
2025-12-10 10:38:32 +0900

    feat: ルーティングシステムと基本ページを追加
    ホームページとログインページを追加し、ルーティングシステムを実装しました。
    main.dartを簡素化し、新しいルーターウィジェットを使用するように変更しました。

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,17 @@
+﻿import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  Loginpage({Key? key}) : super(key: key);
+  @override
+  _LoginpageState createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("登录")),
+      body: Text("登录"),
+    );
+  }
+}
---
+++ lib/routers/index.dart
@@ -0,0 +1,11 @@
+﻿import 'package:flutter/material.dart';
+import 'package:my_app/pages/HomePage.dart';
+import 'package:my_app/pages/LoginPage.dart';
+
+Widget getRouterWidget() {
+  return MaterialApp(initialRoute: '/', routes: getRouterMap());
+}
+
+Map<String, WidgetBuilder> getRouterMap() {
+  return {'/': (context) => Homepage(), '/login': (context) => Loginpage()};
+}
